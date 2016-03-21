4b3517bbc19e971281dfc4c150be4e2d712c542a
Tomas Barry <tomas.barry@hotmail.com>
2016-03-21 22:32:25 +0000

    Cleaned up file browser activity

--- Giggle/app/src/main/java/tomas/giggle/FileBrowserActivity.java
+++ Giggle/app/src/main/java/tomas/giggle/FileBrowserActivity.java
@@ -23,67 +23,40 @@ public class FileBrowserActivity extends AppCompatActivity {
     private ListView list;
     private String[] fileNames = null;
     ArrayList<DropboxAPI.Entry> dropboxFiles;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_browser);
 
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                fileNames = getFilesArray();
-            }
-        });
-        try {
-            while (fileNames == null) {
-                Log.i("Giggle_FBA_onCreate", "Waiting");
-                Thread.sleep(250);
-            }
-        }
-        catch (Exception e) {
-            Log.e("Giggle_FBA_onCreate", "Exception: " + e.toString(), e);
-        }
+        setUpFilesView();
+        initList();
+        initBuilder();
 
+    }
+
+    public void initBuilder() {
         DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 switch (which){
                     case DialogInterface.BUTTON_POSITIVE:
                         // Edit button
+                        Log.i("initBuilder", "User wants to edit file");
                         break;
 
                     case DialogInterface.BUTTON_NEGATIVE:
                         // Delete Button
+                        Log.i("initBuilder", "User wants to delete file");
                         break;
                 }
             }
         };
 
-        final AlertDialog.Builder builder = new AlertDialog.Builder(FileBrowserActivity.this);
+        builder = new AlertDialog.Builder(FileBrowserActivity.this);
         builder.setMessage("What do you want to do?").setPositiveButton("Edit", dialogClickListener)
                 .setNegativeButton("Delete", dialogClickListener);
-
-
-        list = (ListView)findViewById(R.id.fileBrowserList);
-        list.setAdapter(
-                new ArrayAdapter<String>(
-                        this,
-                        R.layout.file_browser_row, //use this layout as a ListView row
-                        R.id.filenametext, //place the String data into this Textview
-                        fileNames) //get info from this array
-        );
-
-        //set up ListView OnClick Listener, this is for the row, not the button on the list row
-        list.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
-                        Log.i("Giggle_FBA_onCreate", "File clicked: " + fileNames[myItemInt]);
-                        builder.show();
-                    }
-                }
-        );
     }
 
     public String[] getFilesArray() {
@@ -105,5 +78,45 @@ public class FileBrowserActivity extends AppCompatActivity {
             Log.e("getFilesArray", "Exception: " + e.toString(), e);
         }
         return fnames;
+    }
+
+    public void setUpFilesView() {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                fileNames = getFilesArray();
+            }
+        });
+        try {
+            while (fileNames == null) {
+                Log.i("setUpFilesView", "Waiting");
+                Thread.sleep(250);
+            }
+        }
+        catch (Exception e) {
+            Log.e("setUpFilesView", "Exception: " + e.toString(), e);
+        }
+    }
+
+    public void initList() {
+        list = (ListView)findViewById(R.id.fileBrowserList);
+        list.setAdapter(
+                new ArrayAdapter<String>(
+                        this,
+                        R.layout.file_browser_row, //use this layout as a ListView row
+                        R.id.filenametext, //place the String data into this Textview
+                        fileNames) //get info from this array
+        );
+
+        //set up ListView OnClick Listener, this is for the row, not the button on the list row
+        list.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
+                        Log.i("initList", "File clicked: " + fileNames[myItemInt]);
+                        builder.show();
+                    }
+                }
+        );
     }
 }
