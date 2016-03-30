319dca91e93037f767dab247bd65f325409a6201
Tomas Barry <tomas.barry@hotmail.com>
2016-03-30 14:06:17 +0100

    Symmetric Key handler created and working

---
+++ Giggle/app/src/main/java/tomas/giggle/SymmetricKeyHandler.java
@@ -0,0 +1,56 @@
+package tomas.giggle;
+
+import android.content.Context;
+import android.util.Log;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Created by Tomas on 30/03/2016.
+ */
+public class SymmetricKeyHandler {
+
+    private Context context;
+    private SecretKey secretKey;
+    private SecretKeySpec secretKeySpec;
+
+    private byte [] binaryDataKey;
+
+    public SymmetricKeyHandler(Context context) {
+        this.context = context;
+        this.binaryDataKey = generateNewSymmetricKey();
+    }
+
+    public SymmetricKeyHandler(Context context, byte [] binaryDataKey) {
+        this.context = context;
+        this.binaryDataKey = binaryDataKey;
+    }
+
+    public byte [] generateNewSymmetricKey() {
+        Log.i("generateNewSymmetricKey", "About to generate a new symmetric key");
+        try {
+            // 1. Generate a session key
+            javax.crypto.KeyGenerator keyGen = javax.crypto.KeyGenerator.getInstance("AES");
+            keyGen.init(128);
+            SecretKey sessionKey = keyGen.generateKey();
+            SecretKeySpec secretKeySpec = new SecretKeySpec(sessionKey.getEncoded(), "AES");
+
+            // 2. Encrypt the session key with the RSA public key
+            Cipher rsaCipher = Cipher.getInstance("RSA");
+            rsaCipher.init(Cipher.ENCRYPT_MODE, new tomas.giggle.KeyGenerator(context).getPublicKey());
+            byte [] encryptedSessionKey = rsaCipher.doFinal(secretKeySpec.getEncoded());
+
+            Base64Translator trans = new Base64Translator(context);
+            String stringOfBinary = trans.fromBinary(encryptedSessionKey);
+            Log.i("generateNewSymmetricKey", "Generated new symmetric key: " + stringOfBinary);
+            return encryptedSessionKey;
+        } catch (Exception e) {
+            Log.e("generateNewSymmetricKey", e.toString(), e);
+        }
+        Log.i("generateNewSymmetricKey", "Symmetric key not generated, returning null");
+        return null;
+    }
+
+}
--- Giggle/app/src/main/java/tomas/giggle/UserPermissionsViewer.java
+++ Giggle/app/src/main/java/tomas/giggle/UserPermissionsViewer.java
@@ -2,19 +2,12 @@ package tomas.giggle;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Hashtable;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 public class UserPermissionsViewer extends Activity {
 
@@ -55,51 +48,27 @@ public class UserPermissionsViewer extends Activity {
 
         Log.i("onCreate_UPV", "Ending on create");
 
-        try {
-            // 1. Generate a session key
-            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-            keyGen.init(128);
-            SecretKey sessionKey = keyGen.generateKey();
-            SecretKeySpec secretKeySpec = new SecretKeySpec(sessionKey.getEncoded(), "AES");
+        SymmetricKeyHandler h = new SymmetricKeyHandler(this);
+        SymmetricKeyHandler j = new SymmetricKeyHandler(this,
+                new Base64Translator(this).toBinary(MainActivity.databaseController.getEncKeyFor(fileNameString, userPublicKey)));
 
-            // 2. Encrypt the session key with the RSA public key
-            Cipher rsaCipher = Cipher.getInstance("RSA");
-            rsaCipher.init(Cipher.ENCRYPT_MODE, new tomas.giggle.KeyGenerator(this).getPublicKey());
-            byte[] encryptedSessionKey = rsaCipher.doFinal(secretKeySpec.getEncoded());
-            String x = Base64.encodeToString(encryptedSessionKey, Base64.NO_WRAP);
-
-            Log.i("onCreate_UPV", "Enc Session Key: " + x);
-
-            Base64Translator trans = new Base64Translator(this);
-            String stringOfBinary = trans.fromBinary(encryptedSessionKey);
-            Log.i("pzl", "Converting to string: " + stringOfBinary);
-            byte[] bytesOfBinary = trans.toBinary(stringOfBinary);
-            Log.i("pzl", "Converting from binary: " + bytesOfBinary);
-
-            Log.i("pzl", "Are they equal in length: " + (encryptedSessionKey.length == bytesOfBinary.length));
-            Log.i("pzl", "Are they equal tho: " + (Arrays.equals(encryptedSessionKey, bytesOfBinary)));
-
-        } catch (Exception e) {
-            Log.e("onCreate_UPV", e.toString(), e);
-        }
-
-        EncryptionKey encryptionKeyBefore =
-                new EncryptionKey(
-                        MainActivity.databaseController.getEncKeyFor(fileNameString, userPublicKey),
-                        userPublicKey, false, this);
-        Log.i("addPermissionFor", "encryptionKey.plainKey " + encryptionKeyBefore.getPlainKey());
-        Log.i("addPermissionFor", "encryptionKey.encryptedKey " + encryptionKeyBefore.getEncryptedKey());
-        Log.i("addPermissionFor", "encryptionKey.publicKey " + encryptionKeyBefore.getPublicKey());
-
-        EncryptionKey encryptionKeyAfter =
-                new EncryptionKey(
-                        encryptionKeyBefore.getEncryptedKey(),
-                        new tomas.giggle.KeyGenerator(this).getPrivateKeyAsString(), true, this);
-        Log.i("addPermissionFor", "encryptionKey.decryptedKey " + encryptionKeyAfter.getDecryptedKey());
-        Log.i("addPermissionFor", "encryptionKey.encryptedKey " + encryptionKeyAfter.getEncryptedKey());
-        Log.i("addPermissionFor", "encryptionKey.privateKey " + encryptionKeyAfter.getPrivateKey());
-
-        Log.i("PZL", "Are dey eqwal decrypted?? " + encryptionKeyBefore.getPlainKey().equals(encryptionKeyAfter.getDecryptedKey()));
-        Log.i("PZL", "Are dey eqwal encryted??? " + encryptionKeyBefore.getEncryptedKey().equals(encryptionKeyAfter.getEncryptedKey()));
+//        EncryptionKey encryptionKeyBefore =
+//                new EncryptionKey(
+//                        MainActivity.databaseController.getEncKeyFor(fileNameString, userPublicKey),
+//                        userPublicKey, false, this);
+//        Log.i("addPermissionFor", "encryptionKey.plainKey " + encryptionKeyBefore.getPlainKey());
+//        Log.i("addPermissionFor", "encryptionKey.encryptedKey " + encryptionKeyBefore.getEncryptedKey());
+//        Log.i("addPermissionFor", "encryptionKey.publicKey " + encryptionKeyBefore.getPublicKey());
+//
+//        EncryptionKey encryptionKeyAfter =
+//                new EncryptionKey(
+//                        encryptionKeyBefore.getEncryptedKey(),
+//                        new tomas.giggle.KeyGenerator(this).getPrivateKeyAsString(), true, this);
+//        Log.i("addPermissionFor", "encryptionKey.decryptedKey " + encryptionKeyAfter.getDecryptedKey());
+//        Log.i("addPermissionFor", "encryptionKey.encryptedKey " + encryptionKeyAfter.getEncryptedKey());
+//        Log.i("addPermissionFor", "encryptionKey.privateKey " + encryptionKeyAfter.getPrivateKey());
+//
+//        Log.i("PZL", "Are dey eqwal decrypted?? " + encryptionKeyBefore.getPlainKey().equals(encryptionKeyAfter.getDecryptedKey()));
+//        Log.i("PZL", "Are dey eqwal encryted??? " + encryptionKeyBefore.getEncryptedKey().equals(encryptionKeyAfter.getEncryptedKey()));
     }
 }
